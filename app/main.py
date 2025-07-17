11afe54fe9c9a2f0307e24f8061fa164bccdeeb7
AdeptuesMech <masteralbatoros@gmail.com>
2025-07-17 04:28:02 +0330

    test push (#1)
    * test push
    
    * fixed env path used within loadenv()

--- app/main.py
+++
@@ -1,5 +0,0 @@
-# ❗ TODO : Import Uvicorn and use run method to run the uvicorn when ever this file has been executed
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run(app="",host="", port="", reload="")
